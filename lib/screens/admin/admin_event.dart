d43b3ba68fe4997d1e6e9404cda1b78c5d52c85a
xoflo <xordelish@gmail.com>
2026-01-20 19:27:08 +0800

    Jan 20

--- lib/screens/admin/admin_event.dart
+++ lib/screens/admin/admin_event.dart
@@ -1,3 +1,4 @@
+import 'package:event_app/const.dart';
 import 'package:flutter/material.dart';
 
 class EventScreen extends StatefulWidget {
@@ -10,6 +11,31 @@ class EventScreen extends StatefulWidget {
 class _EventScreenState extends State<EventScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: secondaryColor,
+        title: Text('Event: Event Name', style: TextStyle(fontWeight: FontWeight.w700),),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            titleWidget()
+
+
+          ],
+            ),
+      ),
+    );
+  }
+
+
+  titleWidget() {
+    return [
+      Text("Event Name", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),),
+      Text("Status: Ongoing")
+    ];
   }
 }
